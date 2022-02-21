5bd77122dd7cacc586dc6883509806874a9d369e
Trey Kemp <kemptrey@outlook.com>
2022-02-21 15:22:19 -0700

    counter working

--- src/App.js
+++ src/App.js
@@ -2,9 +2,20 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 class App extends Component {
+  // constructor is a special method for creating and initializing an object created with a class
   constructor(props) {
     super(props);
     this.state = { value: 0 };
+    this.inc = this.inc.bind(this);
+    this.dec = this.dec.bind(this);
+  }
+
+  inc() {
+    this.setState({ value: this.state.value + 1 });
+  }
+
+  dec() {
+    this.setState({ value: this.state.value - 1 });
   }
 
   render() {
@@ -12,8 +23,8 @@ class App extends Component {
       <div className="App">
         <h1 className="App-title">Counter Example</h1>
         <p>{this.state.value}</p>
-        <button>+</button>
-        <button>-</button>
+        <button onClick={this.inc}>+</button>
+        <button onClick={this.dec}>-</button>
       </div>
     );
   }
